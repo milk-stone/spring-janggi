1c2a72115ec0984990afa69715b7892302f45b33
milk-stone <ywshero@naver.com>
2025-11-17 21:57:46 +0900

    feat: setup project

---
+++ src/main/java/com/woowa/open_mission/spring_janggi/SpringJanggiApplication.java
@@ -0,0 +1,13 @@
+package com.woowa.open_mission.spring_janggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJanggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJanggiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woowa/open_mission/spring_janggi/SpringJanggiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowa.open_mission.spring_janggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJanggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
