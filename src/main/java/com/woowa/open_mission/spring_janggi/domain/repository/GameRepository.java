93c74f7441dbe610abc82bf51e2b3a1fbc1d1a31
milk-stone <ywshero@naver.com>
2025-11-21 02:16:19 +0900

    chore: 장기 게임 구현을 위해 필요한 빈 클래스 파일 생성

---
+++ src/main/java/com/woowa/open_mission/spring_janggi/domain/repository/GameRepository.java
@@ -0,0 +1,4 @@
+package com.woowa.open_mission.spring_janggi.domain.repository;
+
+public interface GameRepository {
+}
---
+++ src/main/java/com/woowa/open_mission/spring_janggi/service/JanggiService.java
@@ -0,0 +1,4 @@
+package com.woowa.open_mission.spring_janggi.service;
+
+public class JanggiService {
+}
