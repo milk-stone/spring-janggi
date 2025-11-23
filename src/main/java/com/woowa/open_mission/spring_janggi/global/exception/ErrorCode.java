d20a97a6952a8e30fd7c1ac925667919233ea4eb
milk-stone <ywshero@naver.com>
2025-11-23 21:58:07 +0900

    feat: Game 엔티티 관련 에러 코드 추가

--- src/main/java/com/woowa/open_mission/spring_janggi/global/exception/ErrorCode.java
+++ src/main/java/com/woowa/open_mission/spring_janggi/global/exception/ErrorCode.java
@@ -14,6 +14,10 @@ public enum ErrorCode {
     INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "ME_002", "비밀번호로 유효하지 않은 값이 입력되었습니다."),
     INVALID_NICKNAME(HttpStatus.BAD_REQUEST, "ME_003", "닉네임으로 유효하지 않은 값이 입력되었습니다.");
 
+    INVALID_NICKNAME(HttpStatus.BAD_REQUEST, "ME_003", "닉네임으로 유효하지 않은 값이 입력되었습니다."),
+    // * Game
+    INVALID_GAME_STATUS(HttpStatus.BAD_REQUEST, "GE_001", "게임이 진행 중인 상태가 아닙니다."),
+    GAME_NOT_FOUND(HttpStatus.NOT_FOUND, "GE_002", "해당 게임이 존재하지 않습니다."),
     private final HttpStatus status;
     private final String code;
     private final String message;
